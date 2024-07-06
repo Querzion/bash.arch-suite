e8e0ad82ba669df1149dd42d17138c62c794d428
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-07-06 02:20:18 +0200

    Update install.dwm-arch.sh

--- install.dwm-arch.sh
+++ install.dwm-arch.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Path to the applications list
-APPLICATIONS_FILE="~/files/applications.txt"
+APPLICATIONS_FILE="$HOME/files/applications.txt"
+XINITRC_FILE="$HOME/.xinitrc"
 
 # Check if the applications file exists
 if [[ ! -f $APPLICATIONS_FILE ]]; then
@@ -16,5 +17,30 @@ while IFS= read -r app; do
         sudo pacman -S --noconfirm "$app"
     fi
 done < "$APPLICATIONS_FILE"
+
+# Install dwm
+echo "Installing dwm..."
+
+# Clone the dwm repository
+git clone https://git.suckless.org/dwm ~/dwm
+
+# Navigate to the dwm directory and compile dwm
+cd ~/dwm
+sudo make clean install
+
+echo "dwm installed successfully."
+
+# Create .xinitrc file in the home directory with specific content
+echo "Creating .xinitrc file in the home directory..."
+
+cat <<EOL > $XINITRC_FILE
+setxkbmap -layout se -variant dvorak &
+/usr/bin/pipewire &
+/usr/bin/pipewire-pulse &
+/usr/bin/pipewire-media-session &
+exec dwm
+EOL
+
+echo ".xinitrc file created successfully."
 
 echo "All applications installed successfully."
