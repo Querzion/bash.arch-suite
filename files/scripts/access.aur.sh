e2b377951e9eb74445bef0143f8f0c5ada1fefb5
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-07-07 20:23:29 +0200

    Update access.aur.sh

--- files/scripts/access.aur.sh
+++ files/scripts/access.aur.sh
@@ -4,6 +4,10 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
+# LOCATION
+DIR="$HOME/bash.dwm-arch.setup/files/scripts"
+
+
 # Function to check if a command exists
 command_exists() {
     command -v "$1" >/dev/null 2>&1
@@ -14,14 +18,14 @@ if ! command_exists paru && ! command_exists yay; then
     echo "Neither paru nor yay is installed."
     read -p "${RED}Do you want to install paru?${NC} (Y/y to install): " install_paru_choice
     if [[ "$install_paru_choice" == "Y" || "$install_paru_choice" == "y" ]]; then
-        chmod +x install.paru.sh
-        ./install.paru.sh
+        chmod +x $DIR/install.paru.sh
+        ./$DIR/install.paru.sh
     fi
 
     read -p "${RED}Do you want to install yay?${NC} (Y/y to install): " install_yay_choice
     if [[ "$install_yay_choice" == "Y" || "$install_yay_choice" == "y" ]]; then
-        chmod +x install.yay.sh
-        ./install.yay.sh
+        chmod +x $DIR/install.yay.sh
+        ./$DIR/install.yay.sh
     fi
 
     if ! command_exists paru && ! command_exists yay; then
