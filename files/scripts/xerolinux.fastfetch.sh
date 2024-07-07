1041f255d082fafee981a6fd3a171bb1188b5068
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-07-07 18:37:08 +0200

    Update xerolinux.fastfetch.sh

--- files/scripts/xerolinux.fastfetch.sh
+++ files/scripts/xerolinux.fastfetch.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# ANSI color codes
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 # Function to check if a command exists
 command_exists() {
     command -v "$1" >/dev/null 2>&1
@@ -8,13 +12,13 @@ command_exists() {
 # Check if paru or yay is installed
 if ! command_exists paru && ! command_exists yay; then
     echo "Neither paru nor yay is installed."
-    read -p "Do you want to install paru? (Y/y to install): " install_paru_choice
+    read -p "{RED}Do you want to install paru?{NC} (Y/y to install): " install_paru_choice
     if [[ "$install_paru_choice" == "Y" || "$install_paru_choice" == "y" ]]; then
         chmod +x install.paru.sh
         ./install.paru.sh
     fi
 
-    read -p "Do you want to install yay? (Y/y to install): " install_yay_choice
+    read -p "{RED}Do you want to install yay?{NC} (Y/y to install): " install_yay_choice
     if [[ "$install_yay_choice" == "Y" || "$install_yay_choice" == "y" ]]; then
         chmod +x install.yay.sh
         ./install.yay.sh
