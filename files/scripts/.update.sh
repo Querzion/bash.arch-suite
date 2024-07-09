5b18cdfa06dce9f477b1e629919111f002f9f523
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-07-09 05:19:39 +0200

    Update .update.sh

--- files/scripts/.update.sh
+++ files/scripts/.update.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-flatpak update
-sudo pacman -Syyu
-sudo yay -Syu
+# ANSI color codes
+
+YELLOW='\033[93m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo -e "${YELLOW} UPDATING PACMAN APPLICATIONS. ${NC}"
+sudo pacman -Syyu -y
+echo -e "${GREEN} DONE UPDATING. ${NC}"
+
+echo -e "${YELLOW} UPDATING YAY/AUR APPLICATIONS. ${NC}"
+sudo yay -Syu -y
+echo -e "${GREEN} DONE UPDATING. ${NC}"
+
+echo -e "${YELLOW} UPDATING FLATPAK APPLICATIONS. ${NC}"
+flatpak update -y
+echo -e "${GREEN} DONE UPDATING. ${NC}"
