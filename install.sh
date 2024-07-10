c5d1a89e93a938acfc1ffbdce289c1f5d7ef64d0
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-07-10 03:50:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,6 +39,55 @@ SAMBA="$CUT/samba"
 chmod +x -R $SCRIPT_FILES
 
 
+################################################################### ADD ARCO LINUX REPO'S
+############ ADD ARCO LINUX REPO's
+
+# Function to add repositories to /etc/pacman.conf
+add_repositories() {
+    sudo tee -a /etc/pacman.conf > /dev/null <<EOL
+################################## ARCO REPOs
+
+#[arcolinux_repo_testing]
+#SigLevel = PackageRequired DatabaseNever
+#Include = /etc/pacman.d/arcolinux-mirrorlist
+
+[arcolinux_repo]
+SigLevel = PackageRequired DatabaseNever
+Include = /etc/pacman.d/arcolinux-mirrorlist
+
+[arcolinux_repo_3party]
+SigLevel = PackageRequired DatabaseNever
+Include = /etc/pacman.d/arcolinux-mirrorlist
+
+[arcolinux_repo_xlarge]
+SigLevel = PackageRequired DatabaseNever
+Include = /etc/pacman.d/arcolinux-mirrorlist
+EOL
+}
+
+# Function to update pacman package lists
+update_pacman() {
+    sudo pacman -Sy
+}
+
+# Main script execution starts here
+echo "Adding Arcolinux repositories to Pacman configuration..."
+
+# Check if script is run with sudo
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root (sudo)." 
+    exit 1
+fi
+
+# Add repositories
+add_repositories
+
+# Update Pacman
+update_pacman
+
+echo "Arcolinux repositories added successfully."
+
+
 ################################################################### UPDATE MIRROR LIST
 ############ UPDATE THE MIRROR LIST (Needs some work done before it goes public.)
 # SETTINGS
