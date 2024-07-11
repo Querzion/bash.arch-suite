1467dc5ce370d4e890ce86fb8583de14b1311e80
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-07-11 07:08:10 +0200

    I wonder if I'm ever going to be done with this because it's becoming a bit too big already, and isn't near to finished. "/

--- files/scripts/dwm/dwm-querzion/install.dwm-querzion.sh
+++ files/scripts/dwm/dwm-querzion/install.dwm-querzion.sh
@@ -1,129 +1,54 @@
+#!/bin/bash
+
+# This script downloads a fresh empty suckless dwm session if you so want
+# but it also extends to adding patches (which can be changed) and my personal
+# config.def.h file and possible other files.
+
+############ COLOURED BASH TEXT
+
+# ANSI color codes
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+PURPLE='\033[0;35m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+################################################################### FILE & FOLDER PATHS
+############ FILE & FOLDER PATHS
+
+# Script Locations
+FOLDER="bash.dwm-arch.startup"  # If 'bash.dwm-arch.startup' is renamed in any way.
+LOCATION="$HOME/$FOLDER/files/scripts/dwm/dwm-querzion/parts"
+
+
+
+################################################################### ST
+############ DWM
+
+sh $LOCATION/dwm.sh
 
 
 ################################################################### ST
 ############ ST
 
-echo -e "${PURPLE} Installing st. ${NC}"
-
-# Install st (suckless terminal)
-echo "Installing st (suckless terminal)..."
-git clone https://git.suckless.org/st $DIR/$USER/st
-cd $DIR/$USER/st
-sudo make clean install
-
-echo -e "${GREEN} st installed successfully. ${NC}"
-
-echo -e "${PURPLE} Configuring config.def.h file. ${NC}"
-
-# Copy to replace config.def.h
-#sudo mv ~/.config/st/config.def.h ~/.config/st/config.def.h.bak
-sudo cp $CONF_F/st/config.def.h ~/.config/st/config.def.h.new
-
-cd $DIR/$USER/st
-
-sudo rm config.h
-sudo make clean install
-
-echo -e "${GREEN} st is now reconfigured. ${NC}"
-
-echo -e "${PURPLE} Patching st. ${NC}"
-
-# Patch st
-sh $PATCH_ST
-
-echo -e "${GREEN} st is now patched. ${NC}"
-
-# Pause the script
-#echo -e "${GREEN} PRESS ENTER TO CONTINUE. ${NC}"
-#read
+sh $LOCATION/st.sh
 
 
 ################################################################### DMENU
 ############ DMENU
 
-echo -e "${GREEN} Installing dmenu. ${NC}"
-
-# Install dmenu (dynamic menu)
-echo "Installing dmenu (dynamic menu)..."
-git clone https://git.suckless.org/dmenu $DIR/$USER/dmenu
-cd $DIR/$USER/dmenu
-sudo make clean install
-
-echo -e "${GREEN} dmenu installed successfully. ${NC}"
-
-echo -e "${PURPLE} Configuring config.def.h file. ${NC}"
-
-# Copy to replace config.def.h
-#sudo mv ~/.config/dmenu/config.def.h ~/.config/dmenu/config.def.h.bak
-sudo cp $CONF_F/dmenu/config.def.h ~/.config/dmenu/config.def.h.new
-
-cd $DIR/$USER/dmenu
-sudo rm config.h
-
-sudo make clean install
-
-echo -e "${GREEN} dmenu is now reconfigured. ${NC}"
-
-echo -e "${PURPLE} Patching dmenu. ${NC}"
-
-# Patch dmenu
-sh $PATCH_DMENU
-
-echo -e "${GREEN} dmenu is now patched. ${NC}"
-
-# Pause the script
-#echo -e "${GREEN} PRESS ENTER TO CONTINUE. ${NC}"
-#read
+sh $LOCATION/dmenu.sh
 
 
 ################################################################### SLSTATUS
 ############ SLSTATUS
 
-echo -e "${GREEN} Installing slstatus. ${NC}"
-
-# Install slstatus (taskbar status)
-echo "Installing slstatus (Taskbar Status)..."
-git clone https://git.suckless.org/slstatus $DIR/$USER/slstatus
-cd $DIR/$USER/slstatus
-sudo make clean install
-
-echo -e "${GREEN} slstatus installed successfully. ${NC}"
-
-echo -e "${PURPLE} Configuring config.def.h file. ${NC}"
-
-# Copy to replace config.def.h
-#sudo mv ~/.config/slstatus/config.def.h ~/.config/slstatus/config.def.h.bak
-sudo cp $CONF_F/slstatus/config.def.h ~/.config/slstatus/config.def.h.new
-
-cd $DIR/$USER/slstatus
-sudo rm config.h
-
-sudo make clean install
-
-echo -e "${GREEN} slstatus is now reconfigured. ${NC}"
-
-echo -e "${PURPLE} Patching slstatus. ${NC}"
-
-# Patch slstatus
-sh $PATCH_SLSTATUS
-
-echo -e "${GREEN} slstatus is now patched. ${NC}"
-
-# Pause the script
-#echo -e "${GREEN} PRESS ENTER TO CONTINUE. ${NC}"
-#read
+sh $LOCATION/slstatus.sh
 
 
 ################################################################### NNN
 ############ NNN
 
-echo -e "${PURPLE} Installing nnn. ${NC}"
-
-# Install nnn (file manager)
-echo "Installing nnn (file manager)..."
-git clone https://github.com/jarun/nnn $DIR/$USER/nnn
-cd $DIR/$USER/nnn
-sudo make
-sudo make install
-
-echo -e "${GREEN} nnn installed successfully. ${NC}"
+sh $LOCATION/nnn.sh 
---
+++ files/scripts/dwm/dwm-querzion/parts/nnn.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# This script downloads a fresh empty suckless dwm session if you so want
+# but it also extends to adding patches (which can be changed) and my personal
+# config.def.h file and possible other files.
+
+############ COLOURED BASH TEXT
+
+# ANSI color codes
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+PURPLE='\033[0;35m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+############################### SETTINGS
+USER="$(whoami)"
+APP="nnn"
+DESCRIPTON="(Terminal File Browser)"
+FROM_HERE="https://github.com/jarun/$APP"
+
+
+################################################################### FILE & FOLDER PATHS
+############ FILE & FOLDER PATHS
+
+# Script Locations
+FOLDER="bash.dwm-arch.startup"  # If 'bash.dwm-arch.startup' is renamed in any way.
+LOCATION="$HOME/$FOLDER/files"
+
+# Patch Script Location
+PATCH="$LOCATION/scripts/patch/dwm-querzion/install.$APP.patches.sh"
+
+# Configuration Files (Edited config.def.h)
+CONFIG="$LOCATION/settings/.config/$APP/config.def.h"
+
+# Installation Path
+TO_THERE="$HOME/.config/wm/$USER/$APP"
+
+################################################################### BACKUP SETTINGS
+############ BACKUP SETTINGS
+
+backup_APP() {
+
+# Source folder
+SOURCE_FOLDER="$HOME/.config/wm/$USER/$APP"
+
+# Destination folder
+DEST_FOLDER="$HOME/.config/vm"
+
+# Create a timestamp
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+
+# Zip file name with timestamp
+ZIP_FILENAME="$DEST_FOLDER/$USER/$APP-$USER.$TIMESTAMP.zip"
+
+echo -e "${PURPLE} Creating a backup of the prior $APP $DESCRIPTON installation... ${NC}"
+
+# Zip command
+zip -r "$ZIP_FILENAME" "$SOURCE_FOLDER"
+
+# Check if the zip command was successful
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}Folder '$SOURCE_FOLDER' successfully zipped into '$ZIP_FILENAME'.${NC}"
+
+    # Delete source folder
+    rm -rf "$SOURCE_FOLDER"
+    echo -e "${GREEN}Source folder '$SOURCE_FOLDER' deleted.${NC}"
+else
+    echo -e "${RED}Error: Failed to zip the folder.${NC}"
+    exit 1
+fi
+}
+
+
+################################################################### NNN INSTALL
+############ NNN INSTALL
+
+echo -e "${YELLOW} Creating folder for $APP $DESCRIPTON installation... ${NC}"
+# Create folder structure
+mkdir -p $HOME/.config/wm
+mkdir -p $HOME/.config/wm/$USER
+
+# Prompt about backup
+read -p "Do you want to backup prior $APP-$USER install? (y/n): " CHOICE1
+if [ "$CHOICE1" = "y" ] || [ "$CHOICE1" = "Y" ]; then
+    backup_APP
+fi
+
+echo -e "${YELLOW} Getting new $APP $DESCRIPTON source files... ${NC}"
+
+# Install application
+git clone $FROM_HERE $TO_THERE && cd $TO_THERE
+
+echo -e "${YELLOW} Compiling $APP $DESCRIPTON... ${NC}"
+sudo make 
+sudo make install
+
+echo -e "${GREEN} $APP installed successfully. ${NC}"
+
+# Pause the script
+#echo -e "${GREEN} PRESS ENTER TO CONTINUE. ${NC}"
+#read
