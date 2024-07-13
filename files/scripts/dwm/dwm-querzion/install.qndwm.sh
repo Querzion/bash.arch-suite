aaab65d2d52f10748f3d444493289740216e3def
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-07-13 13:23:36 +0200

    Update and rename install.dwm-querzion.sh to install.qndwm.sh

---
+++ files/scripts/dwm/dwm-querzion/install.qndwm.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+###
+###  I created a new repository that has a 'qndwm' install script.
+###  This is what is started here. 
+###
+###
+
+############ COLOURED BASH TEXT
+
+# ANSI color codes
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+PURPLE='\033[0;35m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+################################################################### LOCATION
+
+LOCATION="/var/tmp"
+
+
+################################################################### INSTALL DWM-QUERZION
+############ INSTALL DWM-QUERZION
+
+#  Download qndwm
+git clone https://github.com/querzion/bash.qndwm $LOCATION
+
+#  Make files executable
+chmod +x $LOCATION/bash.qndwm/*
+
+#  Go to directory
+cd $LOCATION/bash.qndwm
+
