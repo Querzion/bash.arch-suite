23b06b9c92873a765b79286a9552646f0adff271
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-07-17 03:09:25 +0200

    added some text.

--- files/scripts/hyprland/hyprland.starter.sh
+++ files/scripts/hyprland/hyprland.starter.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-###
-### https://github.com/mylinuxforwork/hyprland-starter
-###
+### LINKS: >> 
+### GitHub Project:>    https://github.com/mylinuxforwork/hyprland-starter
+### YouTube Video:>     https://www.youtube.com/watch?v=jc-vFSXpZF4
 ###
 ###
 
+# Startup Command
 bash <(curl -s https://raw.githubusercontent.com/mylinuxforwork/hyprland-starter/main/setup.sh)
